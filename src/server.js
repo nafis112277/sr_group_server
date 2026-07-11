6397ea4e71f6da5321b93c7f5de206e965c209de
Sadiqur Rahman Nafis <hossainalijms9@gmail.com>
2026-07-11 08:35:59 +0600

    Add all API endpoints

--- src/server.js
+++ src/server.js
@@ -9,7 +9,16 @@ app.use(express.json());
 app.use("/", apiRoutes);
 
 app.get("/", (req, res) => {
-  res.send("SR Group API is running!");
+  res.send(`
+    <h1>?? SR Group API</h1>
+    <p>API is running! Use these endpoints:</p>
+    <ul>
+      <li><a href="/api/dashboard">/api/dashboard</a></li>
+      <li><a href="/api/skills">/api/skills</a></li>
+      <li><a href="/api/projects">/api/projects</a></li>
+      <li><a href="/api/goals">/api/goals</a></li>
+    </ul>
+  `);
 });
 
 app.listen(port, () => {
